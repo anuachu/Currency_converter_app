170b2fc8712580bb660efc4d4f7be632dbbb0e88
anuachu <anushamohan148@gmail.com>
2022-10-29 17:06:56 +1030

    added remove button

--- src/Favourites.js
+++ src/Favourites.js
@@ -1,10 +1,11 @@
-function Favourites({ fav , changeLayer}) {
+function Favourites({ fav , changeLayer,remove}) {
   return (
     <div className="favorites">
       <section className="fav">
         {fav.map((fav, index) =>
           <p key={index}
           onClick={() => changeLayer(index)}>{fav}
+          <button onClick={() => remove(index)}>X</button>
           </p>
         )}
       </section>
